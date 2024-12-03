8c8b7fe5ff2717a6a942a8457c8254b1b06868a7
khaldmansour <khaldmansour93@gmail.com>
2024-12-03 14:49:45 +0200

    [style] run cs-fix

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
@@ -6,8 +7,8 @@ $dotenv->load();
 
 
 return [
-    'host'     => $_ENV['DB_HOST'] ?? '127.0.0.1',
-    'port'     => $_ENV['DB_PORT'] ?? 3306,
+    'host'     => $_ENV['DB_HOST']     ?? '127.0.0.1',
+    'port'     => $_ENV['DB_PORT']     ?? 3306,
     'database' => $_ENV['DB_DATABASE'] ?? 'course_category',
     'username' => $_ENV['DB_USERNAME'] ?? 'root',
     'password' => $_ENV['DB_PASSWORD'] ?? 'root',
